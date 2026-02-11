1b39f6b088f3e7eb53c28912d81365b2e1a2bcf5
Abhinay-05 <abhinaynnd05@gmail.com>
2026-02-11 23:27:15 +0530

    Refactor Ticket class to include new attributes

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -5,10 +5,79 @@ import java.util.List;
 import java.util.Map;
 
 public class Ticket {
-    private String trainID;
-    private String trainNo;
-    private List<List<Integer>> seats;
-    private Map<String, Time> stationTime;
-    private List<String> stations;
+    private String ticketID;
+
+    private String userId;
+    
+    private String source;
+
+    private String destination;
+
+    private String dateOfTravel;
+
+    private Train train;
+
+    public Ticket(){}
+
+    public Ticket(String ticketID, String userId, String source, String destination, String dateOfTravel, Train train){
+        this.ticketID = ticketID;
+        this.userId = userId;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", ticketID, userId, source, destination, dateOfTravel);
+    }
+
+    public String getTicketID(){
+        return ticketID;
+    }
+
+    public void setTicketId(String ticketID){
+        this.ticketID = ticketID;
+    }
+
+    public String getSource(){
+        return source;
+    }
+
+    public void setSource(String source){
+        this.source = source;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public void setDestination(String destination){
+        this.destination = destination;
+    }
+
+    public String getDateOfTravel(){
+        return dateOfTravel;
+    }
+
+    public void setDateOfTravel(String dateOfTravel){
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public Train getTrain(){
+        return train;
+    }
+
+    public void setTrain(Train train){
+        this.train = train;
+    }
 
 }
